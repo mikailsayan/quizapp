51938a4a0e36dab55d09c6623a9c4168a11c5e0f
Mikail Sayan <msayan@outlook.de>
2022-04-02 17:22:46 +0200

    Add SinglePage Function

--- javascript.js
+++ javascript.js
@@ -1,14 +1,55 @@
 import QuizcardFunctions from "./components/Quizcard/quizcard.js";
 //Wir importieren den Export aus quizcard.js hier her und geben einen Namen
 
-QuizcardFunctions();
+//QuizcardFunctions();
 
-const jsAllBookmarks = document.querySelectorAll(".bookmark-icon");
 //Wir holen uns alle(!) Bookmark Icons mit der Klasse
+//const jsAllBookmarks = document.querySelectorAll(".quiz-card");
 
-jsAllBookmarks.forEach((bookmarkIcon) => {
-    QuizcardFunctions(bookmarkIcon);
-});
-//Wir 
+//jsAllBookmarks.forEach((bookmarkIcon) => {
+//    QuizcardFunctions(bookmarkIcon);
+//}); 
 
+//SinglePageFunction
 
+const jsmainpage = document.querySelector(".main-page");
+const jsbookmarkpage = document.querySelector(".bookmark-page");
+const jscreatepage = document.querySelector(".create-page");
+const jsprofilepage = document.querySelector(".profile-page");
+const jsbuttonmain = document.querySelector(".js-button-main");
+const jsbuttonbookmark = document.querySelector(".js-button-bookmark");
+const jsbuttoncreate = document.querySelector(".js-button-create");
+const jsbuttonprofile = document.querySelector(".js-button-profile");
+
+function showMainPage() {
+    jsmainpage.classList.remove("hidden");
+    jsbookmarkpage.classList.add("hidden");
+    jscreatepage.classList.add("hidden");
+    jsprofilepage.classList.add("hidden");
+}
+
+function showBookmarkPage() {
+    jsmainpage.classList.add("hidden")
+    jsbookmarkpage.classList.remove("hidden")
+    jscreatepage.classList.add("hidden")
+    jsprofilepage.classList.add("hidden")
+}
+
+function showCreatePage() {
+    jsmainpage.classList.add("hidden")
+    jsbookmarkpage.classList.add("hidden")
+    jscreatepage.classList.remove("hidden")
+    jsprofilepage.classList.add("hidden")
+}
+
+function showProfilePage() {
+    jsmainpage.classList.add("hidden")
+    jsbookmarkpage.classList.add("hidden")
+    jscreatepage.classList.add("hidden")
+    jsprofilepage.classList.remove("hidden")
+}
+
+jsbuttonmain.addEventListener("click", showMainPage);
+jsbuttonbookmark.addEventListener("click", showBookmarkPage);
+jsbuttoncreate.addEventListener("click", showCreatePage);
+jsbuttonprofile.addEventListener("click", showProfilePage);
