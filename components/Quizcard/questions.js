f8ffc0d53a70a299f13c65ff396acdff5f1b2ab8
Mikail Sayan <msayan@outlook.de>
2022-04-05 17:13:07 +0200

    Save before Exercise with JS

---
+++ components/Quizcard/questions.js
@@ -0,0 +1,23 @@
+const questionsArray = [
+    {
+        id: "1",
+        isBookmarked: false,
+        question: "Wie oft kannst du <header> verwenden?",
+        answer: "Wie oft du möchtest",
+        tags: ["html", "elements", "basic"]
+    },
+    {
+        id: "2",
+        isBookmarked: false,
+        question: "Wie rufst du die Funktion 'fName' auf?",
+        answer: "fName()",
+        tags: ["javascript", "elements", "basic"]
+    },
+    {
+        id: "3",
+        isBookmarked: false,
+        question: "Was ist eine API?",
+        answer: "Eine Schnittstelle, um externe Daten zu importieren",
+        tags: ["javascript", "elements", "basic"]
+    }
+]
